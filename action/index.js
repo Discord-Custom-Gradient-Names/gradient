c35013fe998e5bbfc563c528c91504f43cf04fc4
AA <aa@bigdumb.gq>
2020-09-12 08:21:06 -0400

    bruh

--- action/index.js
+++ action/index.js
@@ -17,7 +17,8 @@ const { exec } = require('child-process-promise');
   const { number } = data;
   const file = await fs.readFile('./database.css');
   console.log(file);
-  const issuebody = JSON.parse(data.body);
+  console.log(data.body);
+  const issuebody = await JSON.parse(data.body);
   console.log(issuebody);
   let css;
   if (issuebody.angle) {
