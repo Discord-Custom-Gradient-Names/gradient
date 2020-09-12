0a627c0ec9b1b1e7e28bc1e9089418501dbc5291
AA <aa@bigdumb.gq>
2020-09-12 09:03:28 -0400

    heck

--- action/index.js
+++ action/index.js
@@ -7,7 +7,7 @@ const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
 const fs = require('fs').promises;
 const { exec } = require('child-process-promise');
 (async () => {
-  console.log(octokit.users.getAuthenticated());
+  console.log(await octokit.users.getAuthenticated());
   const issuenumber = process.env.event_number;
   console.log(issuenumber);
   const issues = await octokit.issues.get({
