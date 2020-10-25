d2312efb91f5059efc2e867de0b97f470080f04d
AAGaming <aa@bigdumb.gq>
2020-10-24 23:36:11 -0400

    make changing format easier

--- action/commit.js
+++ action/commit.js
@@ -1,34 +1,15 @@
 process.on('unhandledRejection', up => {
   throw up;
 });
+const template = require('./template')
 const fs = require('fs').promises;
 const { exec } = require('child-process-promise');
 (async () => {
-  const cssfile = await fs.readFile('./database.json');
-  const obj = await JSON.parse(cssfile);
-  let props;
+  const db = await fs.readFile('./database.json');
+  const obj = await JSON.parse(db);
   let css = '';
   Object.keys(obj).forEach(e => {
-    if (obj[e].angle) {
-      props = `
-  --name-dummy-transparent: transparent; --name-dummy-1: 1;
-  --name-gradient: ${obj[e].color1}, ${obj[e].color2};
-  --name-gradient-angle: ${obj[e].angle};
-    `;
-    } else {
-      props = `
-  --name-dummy-transparent: transparent; --name-dummy-1: 1;
-  --name-gradient: ${obj[e].color1}, ${obj[e].color2};
-    `;
-    }
-    css += `\n\n/*${obj[e].username}*/
-[user_by_bdfdb*="${e}"],
-[data-user-id*="${e}"] + *,
-[data-author-id*="${e}"],
-[style*="${e}"] + *,
-img[src*="${e}"] + * {
-${props}
-}`;
+    css += template(obj[e], e)
   });
   const newjson = await JSON.stringify(obj, null, 1);
   await fs.writeFile('./database.json', newjson);
