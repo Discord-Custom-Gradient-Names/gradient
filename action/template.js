a8fe3326a22a611cf7013a225455227aa6175293
AAGaming00 <aa@bigdumb.gq>
2020-11-09 08:20:12 -0500

    dmlist, also less laggy selectors

--- action/template.js
+++ action/template.js
@@ -1,13 +1,13 @@
-module.exports = function (obj, id) {
-return `\n\n/*${obj.username}*/
-[user_by_bdfdb*="${id}"],
-.avatar-3EQepX[data-user-id*="${id}"] + *,
-.member-3-YXUe[data-user-id*="${id}"],
-[data-author-id*="${id}"],
+module.exports = (obj, id) =>
+  `\n\n/*${obj.username}*/
+[user_by_bdfdb="${id}"],
+.avatar-3EQepX[data-user-id="${id}"] + *,
+.member-3-YXUe[data-user-id="${id}"],
+.channel-2QD9_O[data-user-id="${id}"],
+[data-author-id="${id}"],
 [style*="${id}"] + *,
 img[src*="${id}"] + * {
   --name-dummy-transparent: transparent; --name-dummy-1: 1;
   --name-gradient: ${obj.color1}, ${obj.color2};
   ${obj.angle ? `--name-gradient-angle: ${obj.angle};` : ''}
 }`;
-}
